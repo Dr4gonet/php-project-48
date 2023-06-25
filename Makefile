3527e0d51e6af7e4c6df0104cfbf3a88891dba0a
Dr4gonet <dr4gonet@yandex.ru>
2023-06-25 21:22:08 +0300

    added Differ.php

--- Makefile
+++ Makefile
@@ -5,4 +5,6 @@ validate:
 lint:
 	composer exec --verbose phpcs -- --standard=PSR12 src bin
 check:
-	./vendor/bin/phpstan analyse --level 5 src bin
+	./vendor/bin/phpstan analyse --level 6 src bin
+fix:
+	phpcbf --standard=PSR12 src bin
---
+++ src/Differ.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Differ\Differ;
+
+function getDataArray(string $data): mixed
+{
+    $dataDecode = json_decode($data, true);
+
+    $dataArray = array_map(function ($value) {
+        if ($value === false) {
+            return 'false';
+        } elseif ($value === true) {
+            return 'true';
+        } elseif (is_null($value)) {
+            return 'null';
+        }
+        return $value;
+    }, $dataDecode);
+
+    return $dataArray;
+}
+
+function genDiff(string $pathToFile1, string $pathToFile2): string
+{
+    $data1 = file_get_contents($pathToFile1);
+    $data2 = file_get_contents($pathToFile2);
+
+    $dataArray1 = getDataArray($data1);
+    $dataArray2 = getDataArray($data2);
+
+    $keys = array_unique(array_merge(array_keys($dataArray1), array_keys($dataArray2)));
+
+    sort($keys, SORT_REGULAR);
+
+    $result = array_reduce($keys, function ($acc, $key) use ($dataArray1, $dataArray2) {
+        if (!array_key_exists($key, $dataArray1)) {
+            $acc[] = '+' . $key . ': ' . $dataArray2[$key];
+        } elseif (!array_key_exists($key, $dataArray2)) {
+            $acc[] = '-' . $key . ': ' . $dataArray1[$key];
+        } elseif ($dataArray1[$key] !== $dataArray2[$key]) {
+            $acc[] = '-' . $key . ': ' . $dataArray1[$key];
+            $acc[] = '+' . $key . ': ' . $dataArray2[$key];
+        } else {
+            $acc[] = ' ' . $key . ': ' . $dataArray1[$key];
+        };
+        return $acc;
+    }, []);
+
+    return '{' . "\n" . implode("\n", $result) . "\n" . '}' . "\n";
+}
+
+
+//$pathToFile1 = __DIR__ . '/../file1.json';
+
+//$pathToFile2 = __DIR__ . '/../file2.json';
+
+
+//print_r(genDiff($pathToFile1, $pathToFile2));
