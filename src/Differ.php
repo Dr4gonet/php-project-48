b3ba9ac9f83555439b78553f81163458a0b917a8
Dr4gonet <dr4gonet@yandex.ru>
2023-07-13 23:17:27 +0300

    added getArrayComparisonTree

--- src/Differ.php
+++ src/Differ.php
@@ -33,6 +33,61 @@ function compareArrays(mixed $array1, mixed $array2): mixed
     return $result;
 }
 
+function getArrayComparisonTree(mixed $array1, mixed $array2): mixed
+{
+    $result = [];
+
+    $keys = array_unique(array_merge(array_keys($array1), array_keys($array2)));
+
+    sort($keys, SORT_REGULAR);
+
+    foreach ($keys as $key) {
+        if (
+            array_key_exists($key, $array1) && array_key_exists($key, $array2)
+            && is_array($array1[$key]) && is_array($array2[$key])
+        ) {
+            $nestedComparison = getArrayComparisonTree($array1[$key], $array2[$key]);
+
+            $result[] = [
+                'key' => $key,
+                'type' => 'immutable',
+                'value' => $nestedComparison,
+            ];
+        } elseif (!array_key_exists($key, $array2)) {
+            $result[] = [
+                'key' => $key,
+                'type' => 'deleted',
+                'value' => $array1[$key],
+            ];
+        } elseif (!array_key_exists($key, $array1)) {
+            $result[] = [
+                'key' => $key,
+                'type' => 'added',
+                'value' => $array2[$key],
+            ];
+        } elseif ($array1[$key] !== $array2[$key]) {
+            $result[] = [
+                'key' => $key,
+                'type' => 'deleted',
+                'value' => $array1[$key],
+            ];
+            $result[] = [
+                'key' => $key,
+                'type' => 'added',
+                'value' => $array2[$key],
+            ];
+        } else {
+            $result[] = [
+                'key' => $key,
+                'type' => 'immutable',
+                'value' => $array1[$key],
+            ];
+        }
+    }
+
+    return $result;
+}
+
 function toString(string $value): string
 {
     return trim(var_export($value, true), "'");
