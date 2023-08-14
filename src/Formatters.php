8e5b806df1a64e0a2f002a166ead9e5d3a8d8926
Dr4gonet <dr4gonet@yandex.ru>
2023-08-14 22:45:44 +0300

    fixed Formatters.php

--- src/Formatters.php
+++ src/Formatters.php
@@ -6,22 +6,6 @@ use function Differ\Formatters\Stylish\getFormat as getFormatStylish;
 use function Differ\Formatters\Plain\getFormat as getFormatPlain;
 use function Differ\Formatters\Json\getFormat as getFormatJson;
 
-function getFormattedArray(mixed $dataArray): mixed
-{
-    return array_map(function ($value) {
-        if ($value === false) {
-            return 'false';
-        } elseif ($value === true) {
-            return 'true';
-        } elseif (is_null($value)) {
-            return 'null';
-        } elseif (is_array($value)) {
-            return getFormattedArray($value); // Рекурсивный вызов для обработки вложенных массивов
-        }
-        return $value;
-    }, $dataArray);
-}
-
 function getFormatter(mixed $diff, string $format): string
 {
     switch ($format) {
